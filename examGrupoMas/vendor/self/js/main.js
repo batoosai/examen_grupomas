7bbd8e77f99ff40b13c0c95036bfdaa9a5bdefa6
Eduardo Galindo <lalogalindo@gmail.com>
2017-07-13 01:10:04 -0500

    parse

--- examGrupoMas/vendor/self/js/main.js
+++ examGrupoMas/vendor/self/js/main.js
@@ -2,12 +2,12 @@ function ajaxRequest(data){
   $.ajax({
     url: './ws.php',
     type: 'post',
-    datatype: 'json',
     data: {
       action: data.action,
       data: data.data
     },
     success: function(res){
+      res = JSON.parse(res)
       if( res.status == true ) {
         location.reload()
       } else {
@@ -16,81 +16,3 @@ function ajaxRequest(data){
     }
   })
 }
-
-$(document)
-.on('click','.addItem',function(ev){
-  vex.dialog.open({
-      unsafeMessage: 'Nuevo Item',
-      input: [
-        '<div class="form-group"><input class="form-control" name="name" placeholder="Nombre"></div>',
-        '<div class="form-group"><input class="form-control" name="buyPrice" placeholder="Precio de Compra"></div>',
-        '<div class="form-group"><input class="form-control" name="sellPrice" placeholder="Precio de Venta"></div>',
-      ].join(''),
-      className: 'vex-theme-plain',
-      buttons: [
-          $.extend({}, vex.dialog.buttons.YES, { text: 'Guardar' }),
-          $.extend({}, vex.dialog.buttons.NO, { text: 'Cancelar' })
-      ],
-      callback: function(data) {
-        if( data ) {
-          ajaxRequest({
-            action: 'addItem',
-            data: data
-          })
-        }
-      }
-  })
-})
-.on('click','.modifyItem',function(ev){
-  var id = $(ev.currentTarget).closest('tr').attr('id'),
-  name = $(ev.currentTarget).closest('tr').find('.name').text(),
-  buyPrice = $(ev.currentTarget).closest('tr').find('.buyPrice').text(),
-  sellPrice = $(ev.currentTarget).closest('tr').find('.sellPrice').text()
-  vex.dialog.open({
-      unsafeMessage: 'Modificar Item',
-      input: [
-        '<input type="hidden" name="id" value="'+id+'">',
-        '<div class="form-group">',
-        '<input class="form-control" name="name" placeholder="Nombre" value="'+name+'">',
-        '</div>',
-        '<div class="form-group">',
-        '<input class="form-control" name="buyPrice" placeholder="Precio de Compra" value="'+buyPrice+'">',
-        '</div>',
-        '<div class="form-group">',
-        '<input class="form-control" name="sellPrice" placeholder="Precio de Venta" value="'+sellPrice+'">',
-        '</div>',
-      ].join(''),
-      className: 'vex-theme-plain',
-      buttons: [
-          $.extend({}, vex.dialog.buttons.YES, { text: 'Guardar' }),
-          $.extend({}, vex.dialog.buttons.NO, { text: 'Cancelar' })
-      ],
-      callback: function(data) {
-        if( data ) {
-          ajaxRequest({
-            action: 'modifyItem',
-            data: data
-          })
-        }
-      }
-  })
-})
-.on('click','.deleteItem',function(ev){
-  var id = $(ev.currentTarget).closest('tr').attr('id')
-  vex.dialog.confirm({
-      unsafeMessage: 'Eliminar Item',
-      className: 'vex-theme-plain',
-      buttons: [
-          $.extend({}, vex.dialog.buttons.YES, { text: 'Guardar' }),
-          $.extend({}, vex.dialog.buttons.NO, { text: 'Cancelar' })
-      ],
-      callback: function(data) {
-        if( data ) {
-          ajaxRequest({
-            action: 'modifyItem',
-            data: data
-          })
-        }
-      }
-  })
-})
