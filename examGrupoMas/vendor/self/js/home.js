8173eb8339b28764e9ad9667ff041c1f5487061f
Eduardo Galindo <lalogalindo@gmail.com>
2017-07-13 02:10:19 -0500

    almacenes

--- examGrupoMas/vendor/self/js/home.js
+++ examGrupoMas/vendor/self/js/home.js
@@ -1,4 +1,19 @@
 $(document)
+.on('click','.addItemsWarehouse',function(ev){
+  var id = $(ev.currentTarget).closest('tr').attr('id')
+  $.ajax({
+    url: './ws.php',
+    type: 'post',
+    data: {
+      action: 'getItems'
+    },
+    success: function(res) {
+      res = JSON.parse(res)
+      if(res.status == true)
+        addToWarehouse(id,res.data)
+    }
+  })
+})
 .on('click','.verItemsWarehouse',function(ev){
   var id = $(ev.currentTarget).closest('tr').attr('id'),
   whname = $(ev.currentTarget).closest('tr').find('.whname').text()
@@ -38,7 +53,47 @@ $(document)
     }
   })
 })
+.on('click','.deleteFromWarehouse',function(ev){
+})
+.on('click','.transferirItemsWarehouse',function(ev){
 
+})
+
+function addToWarehouse(id,data){
+  vex.dialog.open({
+    unsafeMessage: 'Agrega un Item',
+    input: [
+      '<input type="hidden" name="warehouse" value="'+id+'">',
+      createSelect(data),
+      '<input type="text" class="form-control" placeholder="Cantidad" name="quantity">'
+    ].join(''),
+    className: 'vex-theme-plain',
+    buttons: [
+        $.extend({}, vex.dialog.buttons.YES, { text: 'Guardar' }),
+        $.extend({}, vex.dialog.buttons.NO, { text: 'Cancelar' })
+    ],
+    callback: function(data) {
+      if( data ) {
+        var result = ajaxRequest({
+          action: 'addToWarehouse',
+          data: data
+        })
+        if( result == true)
+          location.reload()
+      }
+    }
+  })
+}
+function createSelect(data) {
+  var select = [
+    '<select class="form-control" name="item">'
+  ]
+  for (var i = 0; i < data.length; i++) {
+    select.push('<option value="'+data[i].id+'">'+data[i].name+'</option>')
+  }
+  select.push('</select>')
+  return select.join('')
+}
 function createTable(data) {
   var table = [
   '<table class="table table-hover table-striped">']
@@ -46,7 +101,9 @@ function createTable(data) {
     table.push('<tr id="'+data[i].id+'">')
     table.push('<td>'+data[i].name+'</td>')
     table.push('<td>'+data[i].quantity+'</td>')
-    table.push('<td><button type="button" class="btn btn-danger deleteFromWarehouse"><i class="fa fa-times"></i></button></td>')
+    table.push('<td>')
+    table.push('<button type="button" class="btn btn-danger deleteFromWarehouse"><i class="fa fa-times"></i></button>')
+    table.push('</td>')
     table.push('</tr>')
   }
   table.push('</table>')
