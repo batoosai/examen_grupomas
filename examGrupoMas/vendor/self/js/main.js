f1cf8d686ff4ee7f8267e06f7d20c6faeeadc99f
Eduardo Galindo <lalogalindo@gmail.com>
2017-07-13 00:53:12 -0500

    add modify item

--- examGrupoMas/vendor/self/js/main.js
+++ examGrupoMas/vendor/self/js/main.js
@@ -1,6 +1,6 @@
 function ajaxRequest(data){
   $.ajax({
-    url: data.url,
+    url: './ws.php',
     type: 'post',
     data: {
       action: data.action,
@@ -18,8 +18,8 @@ $(document)
       unsafeMessage: 'Nuevo Item',
       input: [
         '<div class="form-group"><input class="form-control" name="name" placeholder="Nombre"></div>',
-        '<div class="form-group"><input class="form-control" name="precio_compra" placeholder="Precio de Compra"></div>',
-        '<div class="form-group"><input class="form-control" name="precio_venta" placeholder="Precio de Venta"></div>',
+        '<div class="form-group"><input class="form-control" name="buyPrice" placeholder="Precio de Compra"></div>',
+        '<div class="form-group"><input class="form-control" name="sellPrice" placeholder="Precio de Venta"></div>',
       ].join(''),
       className: 'vex-theme-plain',
       buttons: [
@@ -28,8 +28,30 @@ $(document)
       ],
       callback: function(data) {
         ajaxRequest({
-          url: '',
-          action: '',
+          action: 'addItem',
+          data: data
+        })
+      }
+  })
+})
+.on('click','.modifyItem',function(ev){
+  var id = $(ev.currentTarget).closest('tr').attr('id')
+  vex.dialog.open({
+      unsafeMessage: 'Modificar Item',
+      input: [
+        '<input type="hidden" name="id" value="'+id+'">',
+        '<div class="form-group"><input class="form-control" name="name" placeholder="Nombre"></div>',
+        '<div class="form-group"><input class="form-control" name="buyPrice" placeholder="Precio de Compra"></div>',
+        '<div class="form-group"><input class="form-control" name="sellPrice" placeholder="Precio de Venta"></div>',
+      ].join(''),
+      className: 'vex-theme-plain',
+      buttons: [
+          $.extend({}, vex.dialog.buttons.YES, { text: 'Guardar' }),
+          $.extend({}, vex.dialog.buttons.NO, { text: 'Cancelar' })
+      ],
+      callback: function(data) {
+        ajaxRequest({
+          action: 'modifyItem',
           data: data
         })
       }
